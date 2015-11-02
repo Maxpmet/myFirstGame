710f17d3cb335c31b2a5ce0d27455acce4788e6a
tangshicong <435957824@qq.com>
2015-11-02 22:04:04 +0800

    开始写逻辑

--- LianLianKan/src/app/views/GameScene.lua
+++ LianLianKan/src/app/views/GameScene.lua
@@ -8,8 +8,9 @@
 local GameScene = class("GameScene", cc.load("mvc").ViewBase)
 
 local statusTable = nil
-local ROW = 6
-local COLUMN = 6
+local ROW         = nil
+local COLUMN      = nil
+local lastIndex   = nil
 
 function GameScene:onCreate()
 	GameScene.initData()
@@ -66,6 +67,8 @@ function GameScene.initData()
 			4,
 		},
 	}
+	ROW    = #statusTable
+	COLUMN = #statusTable[1]
 end
 
 function GameScene.initUI()
@@ -76,19 +79,28 @@ function GameScene.initUI()
 				local sprite = display.newSprite( "icon/"..data[j]..".png", 80 * j - 40, 80 * (ROW - i) + 40 )
 				sprite:setTag( (i - 1) * COLUMN + j )
 				sprite:setTouchEnabled(true)
-				sprite:addTouchEventListener(GameScene.clickSpriteCallback)
+				sprite:addNodeEventListener(self, GameScene.clickSpriteCallback)
 				this->addChild(sprite)
 			end
 		end
 	end
 end
 
-function GameScene.clickSpriteCallback()
-	
+function GameScene.clickSpriteCallback(pSender, event)
+	if event.name == "began"
+	if lastIndex then
+		-- 第二次点击，判断能否连接
+	else
+		-- 第一次点击
+		lastIndex = sprite
+	end
 end
 
 function GameScene.clean()
 	statusTable = nil
+	ROW         = nil
+	COLUMN      = nil
+	lastIndex 	= nil
 end
 
 return GameScene
